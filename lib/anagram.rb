90b052a3761cf14fc5d4f2fb628c92c877d45c92
Diana Liao <dijali@gmail.com>
2021-02-25 11:49:06 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize (word)
+        @word = word
+    end
+
+    def match (array)
+
+        goal = self.word.chars
+        matches = []
+
+        array.each do |test_word|
+            if test_word.chars.sort == goal.sort
+                matches << test_word
+            end
+        end
+        matches
+    end
+end
